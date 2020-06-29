b225c1fa2b2f1ed090fd7d1701c8b5e35e67fe63
dthuylinhh1 <54965669+dthuylinhh1@users.noreply.github.com>
2020-06-30 03:38:52 +0700

    first commit

--- models/reservation.js
+++ models/reservation.js
@@ -1,44 +1,6 @@
-const mongoose = require('mongoose');
-
-const ReservationSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  restaurant: {
-    type: String,
-    enum: [
-      'Kelseys',
-      'Montanas',
-      'Outbacks',
-      'Harveys',
-      'Swiss Chalet'
-    ],
-    default: 'Kelseys',
-    required: true
-  },
-  dateAndTime: {
-    type: Date,
-    required: true,
-    set: val => {
-      return new Date(val);
-    },
-    get: val => {
-      // return `${val.getFullYear()}-${val.getMonth() + 1}-${val.getDate()}T${val.getHours()}:${val.getMinutes()}:${val.getSeconds()}`;
-      const date = val.toISOString();
-      return date.substring(0, date.length - 1);
-    }
-  },
-  quantityOfGuests: {
-    type: Number,
-    default: 2,
-    required: true
-  }
-}, {
-  timestamps: true
-});
-
-
-
-module.exports = mongoose.model('Reservation', ReservationSchema);
+// You need to define the schema for a reservation
+// The fields you require are:
+// associated user
+// quantity of guests
+// restaurant name
+// date and time of reservation (you can do these as separate fields if you wish) 
